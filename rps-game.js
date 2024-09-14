178430f6b537031db73a29b1d7276423bfc2e9dd
Sambaramba <samuelharrison19@tutanota.com>
2024-09-14 09:05:22 +0100

    feat while loop in playGame() function

--- rps-game.js
+++ rps-game.js
@@ -64,24 +64,23 @@ function playGame() {
 
     let humanScore = 0;
     let computerScore = 0;
-        
-        // let round = 0
-        // while (humanScore < 5 || computerScore < 5) {
 
-        //     if ()
+  
 
-        //FIGURE OUT HOW TO STOP GAME WHEN SCORE GETS TO 5 AND DECLARE WINNER!!//
+        while (humanScore !==5 || computerScore !== 5) {
 
-    // for (let humanScore = 0 && let computerScore = 0; humanScore === 5 || computerScore === 5; i++) {
 
-        while (humanScore !== 5 || computerScore !== 5) {
         
         function playRound(humanChoice, computerChoice) {
         
         // when humanScore === 5 return console.log("HUMAN WINS!")
         //let winner = 5//
 
-         if (humanChoice === "rock" && computerChoice === "scissors") {
+        
+
+        
+
+        if (humanChoice === "rock" && computerChoice === "scissors") {
              console.log("YOU WIN! rock beats scissors")
              ++humanScore
              console.log("Score is Human " + humanScore + ": cpu " + computerScore)
@@ -139,27 +138,11 @@ function playGame() {
             console.log("It's a draw!")
            }
         
-        } //moved end bracket to after playRound function call//
+        }
         
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
         
         playRound(humanSelection, computerSelection)
-    }  
+    }
 }
-
-// }
-
-// if (humanSelection > ComputerSelection) {
-//     ++humanScore
-//     console.log()
-// }
-
-// else if (computerSelection > humanSelection) {
-//     ++computerScore
-//     console.log()
-// }
-
-// else {
-//     console.log("its a draw")
-// }
