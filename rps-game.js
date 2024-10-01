1af8e14146970968ad2561c82e5d19f91b668075
Sambaramba <samuelharrison19@tutanota.com>
2024-10-01 16:55:34 +0100

    Fix playGame() and add multiple feats in it remove while statement,change playRound() if else statement, add score variables updates + playRound() function call x5, add new if else

--- rps-game.js
+++ rps-game.js
@@ -60,87 +60,200 @@ function getHumanChoice() {
 
 
 
-function playGame() {
+function playGame(/*round*/) {
 
     let humanScore = 0;
     let computerScore = 0;
 
   
 
-    if (humanScore === 5) {
-      return console.log("Human Wins!")
-   }
-
-   else if (computerScore === 5) {
-      return console.log("Computer Wins!")
-   }
+   //  rock > scissors && paper > rock && scissors > paper
       
-   else {
-        
-        function playRound(humanChoice, computerChoice) {
         
 
-      
+      // while (humanScore !== 3 || computerScore !== 3) {
 
-        if (humanChoice === "rock" && computerChoice === "scissors") {
+         function playRound(humanChoice, computerChoice) {
+            
+           if (humanChoice === "rock" && computerChoice === "scissors") {
              console.log("YOU WIN! rock beats scissors")
              ++humanScore
+            //  ++round
              console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-             
+             return
+              return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
+             return humanScore && computerScore
+             return ++humanScore
+             if (humanScore === 3) {
+               return console.log("Human Wins!")
+            }
            } 
            
            else if (humanChoice === "rock" && computerChoice === "paper") {
               console.log("YOU LOSE! paper beats rock")
               ++computerScore
+            //   ++round
               console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-              
+              return
+               return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
+              return
+              if (computerScore === 3) {
+               return console.log("Computer Wins!")
+            }
            }
         
            else if (humanChoice === "paper" && computerChoice === "rock") {
               console.log("YOU WIN! paper beats rock")
               ++humanScore
+            //   ++round
               console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-            //   if (humanScore === 5) {
-            //     return console.log("Human Wins!")
-            //  }
+              return
+               return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
+              return
+              if (humanScore === 3) {
+                return console.log("Human Wins!")
+             }
            }
         
            else if( humanChoice === "paper" && computerChoice ==="scissors") {
               console.log("YOU LOSE! scissors beats paper")
               ++computerScore
+            //   ++round
               console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-            //   if (computerScore === 5) {
-            //     return console.log("Computer Wins!")
-            //  }
+              return
+              return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
+              return 
+              if (computerScore === 3) {
+                return console.log("Computer Wins!")
+             }
            }
         
            else if(humanChoice === "scissors" && computerChoice === "rock") {
             console.log("YOU LOSE! rock beats scissors")
             ++computerScore
             console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-            // if (computerScore === 5) {
-            //     return console.log("Computer Wins!")
-            //  }
+            // ++round
+            // return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
+            return
+             
+            if (computerScore === 3) {
+                return console.log("Computer Wins!")
+             }
            }
         
            else if (humanChoice === "scissors" && computerChoice === "paper") {
               console.log("YOU WIN! scissors beats paper")
               ++humanScore
+            //   ++round
               console.log("Score is Human " + humanScore + ": cpu " + computerScore);
-            //   if (humanScore === 5) {
-            //     return console.log("Human Wins!")
-            //  }
+            //   return console.log("Score is Human " + humanScore + ": cpu " + computerScore);
+              return
+              
+              if (humanScore === 3) {
+                return console.log("Human Wins!")
+             }
            }
         
            else {
-            console.log("It's a draw!")
+             console.log("It's a draw!")
+             humanSelection = getHumanChoice();
+             computerSelection = getComputerChoice();
+   
+            playRound(humanSelection, computerSelection)
+            //  return console.log("It's a draw!")
+            
+
            }
         
         }
+
+      //   let round = 0
+        /*change humanSelection+computerSelection to let variable instead of const*/
+
         
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
         
         playRound(humanSelection, computerSelection)
-      }
-   }
+   // }
+
+   // if (round === 5) {
+   //    return console.log("Game Over!")
+   //   } else {
+
+   // humanSelection = getHumanChoice();
+   // computerSelection = getComputerChoice();
+   // playRound(humanSelection, computerSelection)
+   //   }
+
+
+
+   humanSelection = getHumanChoice();
+   computerSelection = getComputerChoice();
+   playRound(humanSelection, computerSelection)
+
+
+
+   humanSelection = getHumanChoice();
+   computerSelection = getComputerChoice();
+   playRound(humanSelection, computerSelection)
+
+
+
+   humanSelection = getHumanChoice();
+   computerSelection = getComputerChoice();
+   playRound(humanSelection, computerSelection)
+
+
+   humanSelection = getHumanChoice();
+   computerSelection = getComputerChoice();
+   playRound(humanSelection, computerSelection)
+
+
+   /*THINK I CAN GET RID OF THE BELOW ELSE STATEMENT (NOW THAT playRound() ELSE STATEMENT HAS RECURSIVE FUNCTION)*/
+
+   if (humanScore > computerScore) {
+      return "You beat the computer!"
+   } else if (computerScore > humanScore) {
+      return "You got beaten by the computer!"
+   } else "The games a draw"
+
+   
+
+   // if (round < 6) {
+   //    // getHumanChoice();
+   //    // getComputerChoice()
+   //    playRound(/*humanSelection, computerSelection*/)
+   // } else {
+   //    return console.log("Game Over!")
+   // }
+
+   // for (let round = 0; round < 6; round++) {
+   //    playRound();
+   // }
+
+   /* VARIABLES BELOW ONLY HOLD playRound() RETURN VALUE. THE FUCNCTION HASN'T BEEN CALLED YET */
+   // let round1 = playRound(); 
+   // let round2 = round1 + playRound();
+   // let round3 = playRound();
+   // let round4 = playRound();
+   // let round5 = playRound();
+
+   /*COULD TRY A SINGLE ROUND VARIABLE WITH FOR LOOP THAT STOPS WHEN ROUND EQUALS 5. */
+   /* EXAMPLE for (let round = 0, round < 5, round++) {playRound()} */
+   /*NEED TO UPDATE ROUND EACHTIME IN playRound() IF ELSE STATEMENT*/
+   /*WITH THIS ABOVE IDEA, WHAT ABOUT DRAWS? MIGHT BE FINE IF ELSE STATEMENT DOES'T UPDATE ROUND OR JUST RETURNS ROUND WITHOUT ++ */
+
+   // if (humanScore == 3) {
+   //    return console.log("Human Wins!")
+   // } else if (computerScore == 3) {
+   //    return console.log("Computer Wins!")
+   // } else {
+   //    getHumanChoice();
+   //    getComputerChoice();
+   //    playRound()}
+
+}
+
+
+// playGame()
