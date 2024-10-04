ffe380d2d6fc838f9b7699187016e5a1b126adbf
Sambaramba <samuelharrison19@tutanota.com>
2024-10-04 13:34:37 +0100

    Style: standardised look of code
    cleaned up code to make it easier to read

--- rps-game.js
+++ rps-game.js
@@ -2,60 +2,45 @@ function getComputerChoice() {
     let computerValue = Math.floor(Math.random() * 3);
 
     if (computerValue === 0) {
-        return "rock"
-    }
+        return "rock";
+    };
 
      else if (computerValue === 1) {
-        return "paper"
-    }
+        return "paper";
+    };
 
     else {
-        return "scissors"
-    }
-}
+        return "scissors";
+    };
+};
 
 function getHumanChoice() {
-    let userAnswer = prompt(" What are you going to choose? rock,paper or scissors?")
+
+    let userAnswer = prompt(" What are you going to choose? rock,paper or scissors?");
     let userInputLowercase = userAnswer.toLowerCase();
 
-    
-
-    // if (userInputLowercase !==  "rock" || "paper"|| "scissors") {
-    //     alert ("That is an unacceptable answer numbnuts, please only type either rock, paper or scissors");
-    //      getHumanChoice()
-
-    // } else {
-    //     return userInputLowercase;
-    // }
 
     while(userInputLowercase !==  "rock" || "paper" || "scissors") {
 
 
-        // userAnswer = prompt("That is an unacceptable answer numbnuts, please only type either rock, paper or scissors")
-        
+      if (userInputLowercase === "rock") { 
+        return "rock";
+      };
+      
+      else if  (userInputLowercase === "paper") {
+         return "paper";
+      };
+     
+      else if (userInputLowercase === "scissors") {
+          return "scissors";
+      };
 
-     if (userInputLowercase === "rock") {
-
-     return "rock"
-
-     }
-     else if  (userInputLowercase === "paper") {
-
-        return "paper"  
-
-     }
-     else if (userInputLowercase === "scissors") {
-
-     return "scissors"
-
-     }
-    else { 
-        userAnswer = prompt("That is an unacceptable answer numbnuts, please only type either rock, paper or scissors")
-        userInputLowercase = userAnswer.toLowerCase()
-        // console.log();
-     }
-    }
-}
+      else { 
+         userAnswer = prompt("That is an unacceptable answer numbnuts, please only type either rock, paper or scissors");
+         userInputLowercase = userAnswer.toLowerCase();
+      };
+    };
+};
 
 
 
@@ -67,160 +52,99 @@ function playGame() {
 
   
 
-   //  rock > scissors && paper > rock && scissors > paper
-      
-        
-
-      // while (humanScore !== 3 || computerScore !== 3) {
-
-         function playRound(humanChoice, computerChoice) {
+    function playRound(humanChoice, computerChoice) {
+                
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            alert ("YOU WIN! rock beats scissors");
+            ++humanScore;
+            alert ("Score is Human " + humanScore + " : cpu " + computerScore);
+            return;
+        };
+                
+        else if (humanChoice === "rock" && computerChoice === "paper") {
+            alert ("YOU LOSE! paper beats rock");
+            ++computerScore;
+            alert ("Score is Human " + humanScore + " : cpu " + computerScore);
+            return;          
+        };
             
-           if (humanChoice === "rock" && computerChoice === "scissors") {
-             alert ("YOU WIN! rock beats scissors")
-             ++humanScore
-            //  ++round
+        else if (humanChoice === "paper" && computerChoice === "rock") {
+            alert ("YOU WIN! paper beats rock");
+            ++humanScore;
             alert ("Score is Human " + humanScore + " : cpu " + computerScore);
-             return
-              return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-             return humanScore && computerScore
-             return ++humanScore
-             if (humanScore === 3) {
-               return console.log("Human Wins!")
-            }
-           } 
-           
-           else if (humanChoice === "rock" && computerChoice === "paper") {
-              alert ("YOU LOSE! paper beats rock")
-              ++computerScore
-            //   ++round
+            return;
+        };
+            
+        else if( humanChoice === "paper" && computerChoice ==="scissors") {
+            alert ("YOU LOSE! scissors beats paper");
+            ++computerScore;
             alert ("Score is Human " + humanScore + " : cpu " + computerScore);
-              return
-               return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-              return
-              if (computerScore === 3) {
-               return console.log("Computer Wins!")
-            }
-           }
-        
-           else if (humanChoice === "paper" && computerChoice === "rock") {
-              alert ("YOU WIN! paper beats rock")
-              ++humanScore
-            //   ++round
-            alert ("Score is Human " + humanScore + " : cpu " + computerScore);
-              return
-               return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-              return
-              if (humanScore === 3) {
-                return console.log("Human Wins!")
-             }
-           }
-        
-           else if( humanChoice === "paper" && computerChoice ==="scissors") {
-              alert ("YOU LOSE! scissors beats paper")
-              ++computerScore
-            //   ++round
-            alert ("Score is Human " + humanScore + " : cpu " + computerScore);
-              return
-              return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-              return 
-              if (computerScore === 3) {
-                return console.log("Computer Wins!")
-             }
-           }
-        
-           else if(humanChoice === "scissors" && computerChoice === "rock") {
-            alert ("YOU LOSE! rock beats scissors")
-            ++computerScore
+            return;
+        };
+            
+        else if(humanChoice === "scissors" && computerChoice === "rock") {
+            alert ("YOU LOSE! rock beats scissors");
+            ++computerScore;
             alert ("Score is Human " + humanScore + ": cpu " + computerScore);
-            // ++round
-            // return console.log("Score is Human " + humanScore + ": cpu " + computerScore)
-            return
-             
-            if (computerScore === 3) {
-                return console.log("Computer Wins!")
-             }
-           }
-        
-           else if (humanChoice === "scissors" && computerChoice === "paper") {
-            alert ("YOU WIN! scissors beats paper")
-              ++humanScore
-            //   ++round
-            alert ("Score is Human " + humanScore + " : cpu " + computerScore);
-              // console.log("Score is Human " + humanScore + ": cpu " + computerScore);
-            //   return console.log("Score is Human " + humanScore + ": cpu " + computerScore);
-              return
-              
-              if (humanScore === 3) {
-                return console.log("Human Wins!")
-             }
-           }
-        
-           else {
-             alert ("It's a draw! play round again")
-             humanSelection = getHumanChoice();
-             computerSelection = getComputerChoice();
-   
-            playRound(humanSelection, computerSelection)
-            //  return console.log("It's a draw!")
+            return;
+        };
             
+        else if (humanChoice === "scissors" && computerChoice === "paper") {
+            alert ("YOU WIN! scissors beats paper");
+            ++humanScore;
+            alert ("Score is Human " + humanScore + " : cpu " + computerScore);
+            return;
+        };
+            
+        else {
+            alert ("It's a draw! play round again")
+            humanSelection = getHumanChoice();
+            computerSelection = getComputerChoice();
+            playRound(humanSelection, computerSelection)
+                }
+            
+    }
 
-           }
         
-        }
-
-      //   let round = 0
-        /*change humanSelection+computerSelection to let variable instead of const*/
-
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
         
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        
-        playRound(humanSelection, computerSelection)
-   // }
-
-   // if (round === 5) {
-   //    return console.log("Game Over!")
-   //   } else {
-
-   // humanSelection = getHumanChoice();
-   // computerSelection = getComputerChoice();
-   // playRound(humanSelection, computerSelection)
-   //   }
+playRound(humanSelection, computerSelection)
 
 
 
-   humanSelection = getHumanChoice();
-   computerSelection = getComputerChoice();
-   playRound(humanSelection, computerSelection)
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
 
 
 
-   humanSelection = getHumanChoice();
-   computerSelection = getComputerChoice();
-   playRound(humanSelection, computerSelection)
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
 
 
 
-   humanSelection = getHumanChoice();
-   computerSelection = getComputerChoice();
-   playRound(humanSelection, computerSelection)
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
 
 
-   humanSelection = getHumanChoice();
-   computerSelection = getComputerChoice();
-   playRound(humanSelection, computerSelection)
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
 
 
    /*THINK I CAN GET RID OF THE BELOW ELSE STATEMENT (NOW THAT playRound() ELSE STATEMENT HAS RECURSIVE FUNCTION)*/
 
-   if (humanScore > computerScore) {
-      return alert ("GAME OVER! You beat the computer!")
-   } 
-   else if (computerScore > humanScore) {
-      return alert("GAME OVER! You got beaten by the computer!")
-   } else {"The games a draw"}
+if (humanScore > computerScore) {
+    return alert ("GAME OVER! You beat the computer!")
+} 
+else if (computerScore > humanScore) {
+    return alert("GAME OVER! You got beaten by the computer!")
+} else {"The games a draw"}
 
-   
+  //  let round = 5
 
   //   for (let i = 0; i < round; i++) {
          
@@ -229,22 +153,6 @@ function playGame() {
   //  playRound(humanSelection, computerSelection)
   //   }
 
-   
-
-   /*COULD TRY A SINGLE ROUND VARIABLE WITH FOR LOOP THAT STOPS WHEN ROUND EQUALS 5. */
-   /* EXAMPLE for (let round = 0, round < 5, round++) {playRound()} */
-   /*NEED TO UPDATE ROUND EACHTIME IN playRound() IF ELSE STATEMENT*/
-   /*WITH THIS ABOVE IDEA, WHAT ABOUT DRAWS? MIGHT BE FINE IF ELSE STATEMENT DOES'T UPDATE ROUND OR JUST RETURNS ROUND WITHOUT ++ */
-
-   // if (humanScore == 3) {
-   //    return console.log("Human Wins!")
-   // } else if (computerScore == 3) {
-   //    return console.log("Computer Wins!")
-   // } else {
-   //   humanSelection = getHumanChoice();
-  //  computerSelection = getComputerChoice();
-  //  playRound(humanSelection, computerSelection)
-  //  }
 
 }
 
